4a46d855cb126fcbf174f899ffa0db33c945ad0d
jah <celt8@yandex.ru>
2022-12-01 13:26:27 +0500

    -unit test

--- app/src/androidTest/java/com/example/realmdatabase/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/realmdatabase/ExampleInstrumentedTest.kt
@@ -1,14 +1,16 @@
 package com.example.realmdatabase
 
+
+
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -32,14 +34,14 @@ class ExampleInstrumentedTest {
             .check(matches(isDisplayed()))
             .perform(click())
 
-        val name = "Nikita"
+        val name = "q"
 
         onView(withId(R.id.etName))
             .check(matches(isDisplayed()))
             .perform(typeText(name))
             .check(matches(withText(name)))
 
-        val surname = "Panchenko"
+        val surname = "w"
 
         onView(withId(R.id.etSurname))
             .check(matches(isDisplayed()))
@@ -57,7 +59,86 @@ class ExampleInstrumentedTest {
             .check(matches(isDisplayed()))
             .perform(click())
 
+//        onView(withId(R.id.rvContacts))
+//            .check(matches(isDisplayed()))
+//        onView(withId(R.id.rvContacts))
+//            .check(matches(isDisplayed()))
+//            .perform(click())
+
+        onView(withId(R.id.rvContacts))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1,
+                clickChildViewWithId(R.id.tvEdit)))
+
+//        onView(withId(R.id.rvContacts))
+//            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(17))
+//            .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(withId(R.id.tvEdit), click()))
+//            .check(matches(isDisplayed()))
+//            .perform(click())
+        val newName = "f"
+
+        onView(withId(R.id.etName))
+            .check(matches(isDisplayed()))
+            .perform(setTextInTextView(newName))
+            .check(matches(withText(newName)))// набирает текст
+
+        val newSurname2 = "f"
+
+        onView(withId(R.id.etSurname))
+            .check(matches(isDisplayed()))
+            .perform(setTextInTextView(newSurname2))
+            .check(matches(withText(newSurname2)))
+
+        val newPhone = "+"
+
+        onView(withId(R.id.etNumber))
+            .check(matches(isDisplayed()))
+            .perform(setTextInTextView(newPhone))
+            .check(matches(withText(newPhone)))
+
+        onView(withId(R.id.btnSaveChanges))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        onView(withId(R.id.fabAddContact))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        val nameToAdd = "sdf"
+
+        onView(withId(R.id.etName))
+            .check(matches(isDisplayed()))
+            .perform(typeText(nameToAdd))
+            .check(matches(withText(nameToAdd)))// набирает текст
+
+        val surnameToAdd = "qwert"
+
+        onView(withId(R.id.etSurname))
+            .check(matches(isDisplayed()))
+            .perform(typeText(surnameToAdd))
+            .check(matches(withText(surnameToAdd)))
+
+        val phoneToAdd = "+7777777777"
+
+        onView(withId(R.id.etNumber))
+            .check(matches(isDisplayed()))
+            .perform(typeText(phoneToAdd))
+            .check(matches(withText(phoneToAdd)))
+
+        onView(withId(R.id.btnSave))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        onView(withId(R.id.rvContacts))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2,
+                clickChildViewWithId(R.id.tvEdit)))
+        // Тест экрана редактирования
+        onView(withId(R.id.btnDelete))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
         onView(withId(R.id.rvContacts))
             .check(matches(isDisplayed()))
+
     }
+
 }
